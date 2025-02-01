5ff70266b0c5561a09b124a9c785c31bc5804c5f
Youri Scheepers <youri.scheepers@hva.nl>
2025-02-01 15:36:52 +0100

    Create view model for home page

---
+++ app/src/main/java/me/partypronl/epoch/viewmodel/HomeViewModel.kt
@@ -0,0 +1,34 @@
+package me.partypronl.epoch.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import me.partypronl.epoch.data.models.TimerModel
+import me.partypronl.epoch.data.services.TimerService
+
+@HiltViewModel
+class HomeViewModel: ViewModel() {
+    private val timerService = TimerService()
+
+    private val _timers = MutableStateFlow<List<TimerModel>>(emptyList())
+    val timers = _timers.asStateFlow()
+
+    private val _loadingTimers = MutableStateFlow(false)
+    val loadingTimers = _loadingTimers.asStateFlow()
+
+    init {
+        loadTimers()
+    }
+
+    private fun loadTimers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _loadingTimers.value = true
+            _timers.value = timerService.getTimers()
+            _loadingTimers.value = false
+        }
+    }
+}
