551e7dfe25d03d9280f240534c8e8eaa29506acb
Youri Scheepers <youri.scheepers@hva.nl>
2025-02-02 18:45:38 +0100

    Disable delete button while deleting

--- app/src/main/java/me/partypronl/epoch/ui/screens/home/DeleteTimerDialog.kt
+++ app/src/main/java/me/partypronl/epoch/ui/screens/home/DeleteTimerDialog.kt
@@ -4,16 +4,19 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.partypronl.epoch.data.models.TimerModel
 import me.partypronl.epoch.viewmodel.HomeViewModel
+import androidx.compose.runtime.getValue
 
 @Composable
 fun DeleteTimerDialog(timer: TimerModel, onClose: () -> Unit, viewModel: HomeViewModel = hiltViewModel()) {
     val scope = rememberCoroutineScope()
+    val deleting by viewModel.deletingTimer.collectAsState()
 
     AlertDialog(
         title = { Text("Delete timer?") },
@@ -23,6 +26,7 @@ fun DeleteTimerDialog(timer: TimerModel, onClose: () -> Unit, viewModel: HomeVie
             onClick = onClose
         ) { Text("Cancel") } },
         confirmButton = { TextButton(
+            enabled = !deleting,
             onClick = {
                 scope.launch(Dispatchers.IO) {
                     viewModel.deleteTimer(timer.id)
