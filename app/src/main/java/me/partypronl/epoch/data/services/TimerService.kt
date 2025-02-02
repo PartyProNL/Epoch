fbbd3b03f7420bd24b74c86afc62bc0856eec826
Youri Scheepers <youri.scheepers@hva.nl>
2025-02-02 15:47:17 +0100

    Add method in TimerService to create timer

--- app/src/main/java/me/partypronl/epoch/data/services/TimerService.kt
+++ app/src/main/java/me/partypronl/epoch/data/services/TimerService.kt
@@ -7,6 +7,16 @@ class TimerService {
     val timerDao = DatabaseManager.instance.db.timerDao()
 
     suspend fun getTimers(): List<TimerModel> {
-        return timerDao.getAll()
+        val timers = timerDao.getAll().toMutableList()
+        timers.add(TimerModel(1L, "Lily comes back", System.currentTimeMillis(), System.currentTimeMillis()+1000*60))
+        timers.add(TimerModel(1L, "Internship starts", System.currentTimeMillis()-1000*60, System.currentTimeMillis()+2000*60))
+        return timers
+
+//        return timerDao.getAll()
+    }
+
+    suspend fun createTimer(name: String, ends: Long) {
+        val timerModel = TimerModel(0L, name, System.currentTimeMillis(), ends)
+        timerDao.insert(timerModel)
     }
 }
